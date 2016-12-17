0d7016d049d3f873329c50f588254b86a20de794
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-17 02:46:46 +0100

    Petites corrections sur l'affichage

--- app/Views/documents/documents.php
+++ app/Views/documents/documents.php
@@ -2,18 +2,10 @@
 	<?= $this->getCategory(); ?>
 </div>
 <div class="bleu">
-	<table>
-		<tr>
-			<td class="titre">Titre</td>
-			<td class="titre">Description</td>
-			<td class="titre">Lien</td>
-		</tr>
 		<?php foreach ($lesDocs as $leDoc): ?>
-		<tr>
-			<td><?= $leDoc->titre; ?></a></td>
-			<td><?= $leDoc->description; ?></td>
-			<td><a class="link" href="?page=documents.<?= $this->getCategory(); ?>.<?= $leDoc->id?>">voir plus</a></td>
-		</tr>
+
+			<label>Titre: </label><div class="titre"><?= $leDoc->titre; ?></div>
+			<label>Lien: </label><div class="lien"><a class="link" href="?page=documents.<?= $this->getCategory(); ?>.<?= $leDoc->id?>">voir plus</a></div>
+			<div class="center">------</div>
 		<?php endforeach; ?>
-	</table>
 </div>
--- app/Views/index/login.php
+++ app/Views/index/login.php
@@ -9,8 +9,8 @@
     </div>
     <?php endif; ?>
 	<form name="login" method="POST" action="">
-		<input type="text" value="" placeholder="Login" name="login">
-		<input type="password" value="" placeholder="Password" name="password">
-		<input type="submit" value="send" name="send">
+		<input class="input" type="text" value="" placeholder="Login" name="login">
+		<input class="input" type="password" value="" placeholder="Password" name="password">
+		<input type="submit" class="button" value="send" name="send">
 	</form>
 </div>
