614253218f8e4915789b4835d1ab1ff4893edef1
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-17 03:37:16 +0100

    Correction du titre

--- config/Config.php
+++ config/Config.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once(ROOT.'/core/Database/PdoMysql.php');
-$titre = 'UnskilledSheep';
+$titre = 'UnskilledSheeps';
 if (Bdd::needInstance())
 {
 	$_SESSION['bdd'] = new Bdd('localhost', 'unskilledsheep', 'root', 'test123', false, null, null);
