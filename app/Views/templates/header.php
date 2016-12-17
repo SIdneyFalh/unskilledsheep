b6f38c5994a41f660c7225eb809ecdbff69429ae
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-17 02:54:27 +0100

    Ajout de la coloration syntaxique

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <title><?= $titre; ?></title>
   <link rel="stylesheet" href="css/style.css">
-  <script src="script.js"></script>
+  <link rel="stylesheet" href="css/prism.css">
+  <script src="js/prism.js"></script>
 </head>
 <body>
