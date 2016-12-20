672495d9350de063b7396f6def4ba16db2bd910d
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-20 01:04:38 +0100

    Modification du css

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,6 +5,7 @@
   <title><?= $titre; ?></title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/prism.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
   <script src="js/prism.js"></script>
 </head>
 <body>
