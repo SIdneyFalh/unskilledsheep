ae47524f84ea4a049870a10b88ca5c884abcc8af
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-20 20:09:33 +0100

    Modif du menu

--- app/Views/templates/menu.php
+++ app/Views/templates/menu.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-default">
-	<ul class="nav navbar-nav">
+	<ul class="nav navbar-nav" role="navigation">
 		<li><a href="?page=index.accueil"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
-		
-		<li><a href="?page=documents.add"><span class="glyphicon glyphicon-plus"></span> Ajouter</a></li>
-		<li><a href="?page=index.disconnect"><span class="glyphicon glyphicon-log-out"></span> Se déconnecter</a></li>
 		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-file"></span> Documents <span class="caret"></span></a>
+			<a href="?page=documents.exploits" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-file"></span> Documents <span class="caret"></span></a>
 			<ul class="dropdown-menu">
 				<li>
 					<a href="?page=documents.exploits">Exploits</a>
 				</li>
           	</ul>
     	</li>
+		<li><a href="?page=documents.add"><span class="glyphicon glyphicon-plus"></span> Ajouter</a></li>
+		<li><a href="?page=index.disconnect"><span class="glyphicon glyphicon-log-out"></span> Se déconnecter</a></li>
+		
     </ul>
 
 </nav>
