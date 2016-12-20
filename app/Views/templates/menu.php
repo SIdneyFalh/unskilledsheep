1a658ba6a75136090aaa3c86a950f59612b722bc
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-20 19:55:36 +0100

    Modif du menu

--- app/Views/templates/menu.php
+++ app/Views/templates/menu.php
@@ -1,9 +1,17 @@
 <nav class="navbar navbar-default">
 	<ul class="nav navbar-nav">
 		<li><a href="?page=index.accueil"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
-		<li><a href="?page=documents.exploits"><span class="glyphicon glyphicon-file"></span> Exploits</a></li>
+		
 		<li><a href="?page=documents.add"><span class="glyphicon glyphicon-plus"></span> Ajouter</a></li>
 		<li><a href="?page=index.disconnect"><span class="glyphicon glyphicon-log-out"></span> Se déconnecter</a></li>
 	</ul>
+	<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-file"></span> Documents <span class="caret"></span></a>
+		<ul class="dropdown-menu">
+			<li>
+				<a href="?page=documents.exploits">Exploits</a>
+			</li>
+          </ul>
+        </li>
 </nav>
 
