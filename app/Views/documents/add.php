ac4de10f2edc43b651265eb20cf7a2e840cab188
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-20 01:52:04 +0100

    Modification du css

--- app/Views/documents/add.php
+++ app/Views/documents/add.php
@@ -9,16 +9,28 @@
     <?php endif; ?>
 	<form name="add" method="POST" ation=""> 
 		<div class="form-group">
-			<label class="add">Titre: </label><input class="form-control" type="text" value="" placeholder="Titre" name="titre"></br>
-			<label class="add">Description: </label><textarea class="form-control" name="description" placeholder="Description"></textarea></br>
-			<label class="add">Contenu: </label><textarea class="form-control" name="contenu" placeholder="Contenu"></textarea></br>
-			<label class="add">Catégorie: </label><input class="form-control" type="text" value="" placeholder="Catégorie" name="categorie"></br>
+			<label class="add">Titre: </label>
+			<input class="form-control" type="text" value="" placeholder="Titre" name="titre">
+		</div>
+		<div class="form-group">
+			<label class="add">Description: </label>
+			<textarea class="form-control" name="description" placeholder="Description"></textarea>
+		</div>
+		<div class="form-group">
+			<label class="add">Contenu: </label>
+			<textarea class="form-control" name="contenu" placeholder="Contenu"></textarea>
+		</div>
+		<div class="form-group">
+			<label class="add">Catégorie: </label>
+			<input class="form-control" type="text" value="" placeholder="Catégorie" name="categorie">
+		</div>
+		<div class="form-group">
 			<label class="add">Type: </label><select class="form-control" name="type">
 				<?php foreach ($lesTypes as $leType): ?>
 					<option value="<?= $leType->id; ?>"><?= Secu::xss($leType->libelle); ?></option>
 				<?php endforeach; ?>
-			</select></br>
+			</select>
 		</div>
-		<input class="btn btn-default" type="submit" name="send" value="send">
+		<input class="btn btn-primary" type="submit" name="send" value="send">
 	</form>
 </div>
--- app/Views/index/register.php
+++ app/Views/index/register.php
@@ -8,7 +8,7 @@
 	<form name="register" method="POST" action="" class="form-inline">
 		<input class="form-control" type="text" value="" placeholder="Login" name="login">
 		<input class="form-control" type="password" value="" placeholder="Password" name="password">
-		<input type="submit" class="btn btn-default" value="send" name="send">
+		<input type="submit" class="btn btn-primary" value="send" name="send">
 		<a class="link" href="?page=index.login">Retour</a>
 	</form>
 </div>
