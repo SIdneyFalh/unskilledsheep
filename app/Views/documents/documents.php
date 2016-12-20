e73e1acced538d2b8561894b0f86ef89bc0bc660
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-20 03:18:37 +0100

    modif de documents et exploits

--- app/Views/documents/documents.php
+++ app/Views/documents/documents.php
@@ -3,18 +3,15 @@
 </div>
 <div class="bleu">
 		<?php foreach ($lesDocs as $leDoc): ?>
-
-			<dl>
-				<dt>Titre: </dt>
-				<dd><?= Secu::xss($leDoc->titre); ?></dd>
-			</dl>
-			<dl>
-				<dt>Lien: </dt>
-				<dd>
-					<a class="link" href="?page=documents.<?= $this->getCategory(); ?>.<?= $leDoc->id?>">
+			<div class="panel panel-default">
+  				<div class="panel-heading">
+  					Titre: <?= Secu::xss($leDoc->titre); ?>
+  				</div>
+  				<div class="panel-body">
+    				Lien: <a class="link" href="?page=documents.<?= $this->getCategory(); ?>.<?= $leDoc->id?>">
 						voir plus
 					</a>
-				</dd>
-			<div class="center">------</div>
+  				</div>
+			</div>
 		<?php endforeach; ?>
 </div>
