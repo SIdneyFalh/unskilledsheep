caf9b394b75f21ab25b16ed49326903526ca0d0f
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-20 02:59:13 +0100

    Modification du menu

--- app/Views/templates/menu.php
+++ app/Views/templates/menu.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-default">
 	<ul class="nav navbar-nav">
-		<li><a href="?page=index.accueil">Accueil</a></li>
-		<li><a href="?page=documents.exploits">Exploits</a></li>
-		<li><a href="?page=documents.add">Ajouter</a></li>
-		<li><a href="?page=index.disconnect">Se déconnecter</a></li>
+		<li><a href="?page=index.accueil"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
+		<li><a href="?page=documents.exploits"><span class="glyphicon glyphicon-file"></span> Exploits</a></li>
+		<li><a href="?page=documents.add"><span class="glyphicon glyphicon-plus"></span> Ajouter</a></li>
+		<li><a href="?page=index.disconnect"><span class="glyphicon glyphicon-log-out"></span> Se déconnecter</a></li>
 	</ul>
 </nav>
 
