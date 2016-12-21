524504530af546974efd5c3f2c9fca3f9c3c57ca
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2016-12-22 00:57:02 +0100

    Modification de jquery

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="css/prism.css">
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<script src="js/prism.js"></script>
+	<script src="js/jquery-3.1.1.min.js"></script>
 	<script src="js/bootstrap.js"></script>
 </head>
 <body>
